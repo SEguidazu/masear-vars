43ccc97ec1f117642e6d0fd0584591be1f401d99
Santiago Eguidazu <eguidazu.san@gmail.com>
2024-10-21 22:42:03 -0300

    primer commit de pruebas

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { ArgetinaMapV2 } from "./assets/map";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <main className="container">
+      <ArgetinaMapV2 />
+      <Button>Hello</Button>
+    </main>
+  );
+}
+
+export default App;
